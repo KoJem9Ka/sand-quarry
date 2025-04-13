b29cba1482dd9212ee872198a9b5959777d4e42e
Biryukov Ilya <bis21080000@gmail.com>
2025-04-14 02:37:26 +0300

    fix: add basePath

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,16 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 
-// const isDev = process.env.NODE_ENV === 'development';
-// const isProd = !isDev;
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
 
-// const basePath = isProd ? '/sand-quarry' : undefined;
+const basePath = isProd ? '/sand-quarry' : undefined;
 
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  // basePath,
-  // env: { BASE_PATH: basePath },
+  basePath,
+  env: { BASE_PATH: basePath },
 } as const satisfies NextConfig;
 
 const withNextIntl = createNextIntlPlugin();
